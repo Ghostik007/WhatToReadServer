954b2be18c9134529d71000d8520de66bffa0540
ghostik007 <ghostik007@gmail.com>
2019-03-24 19:34:06 +0500

    Create TomCat server.

---
+++ src/main/java/com/WhatToRead/Server/Config/WebConfig.java
@@ -0,0 +1,14 @@
+package com.WhatToRead.Server.Config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+@Configuration
+@EnableWebMvc
+@ComponentScan("com.WhatToRead.Server")
+
+public class WebConfig extends WebMvcConfigurerAdapter {
+}
